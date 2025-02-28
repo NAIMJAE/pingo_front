2e9e68e7bdc6433c3e248e3566c73ade9acafe1c
Junhyeok <loveu9911@naver.com>
2025-02-28 17:10:37 +0900

    detail 완료

--- lib/data/view_models/ping_check_view_model/ping_check_view_model.dart
+++ lib/data/view_models/ping_check_view_model/ping_check_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pingo_front/_core/utils/logger.dart';
 import 'package:pingo_front/data/models/keyword_model/keyword.dart';
 import 'package:pingo_front/data/models/main_model/Profile.dart';
 import 'package:pingo_front/data/models/main_model/ProfileDetail.dart';
@@ -9,12 +10,14 @@ class PingCheckViewModel extends Notifier<Map<String, List<Profile>>> {
   final PingCheckRepository _repository;
   PingCheckViewModel(this._repository);
 
+  Future<void> pingcheck(String userNo) async {
+    state = await _repository.checkPing(userNo);
+    logger.i(state);
+  }
+
   @override
   Map<String, List<Profile>> build() {
-    return {
-      "super": _generateProfiles(),
-      "ping": _generateProfiles(),
-    };
+    return {};
   }
 }
 
@@ -22,89 +25,3 @@ final pingCheckViewModelProvider =
     NotifierProvider<PingCheckViewModel, Map<String, List<Profile>>>(
   () => PingCheckViewModel(PingCheckRepository()),
 );
-
-// 임시 데이터
-List<Profile> _generateProfiles() {
-  return [
-    Profile(
-      userNo: 'US12341234',
-      name: '박임재',
-      age: '31',
-      status: '접속 중',
-      distance: '1km 거리',
-      ImageList: [
-        '/images/users/US1d49ccfd/UI6aabdcd4.jpg',
-        '/images/users/US761293e0/UIbdefe04a.jpg',
-        '/images/users/US5f080b30/UI7e8f6be3.jpg'
-      ],
-      profileDetail: ProfileDetail(
-        UserInfo(
-            userNo: 'US12341234',
-            user1stJob: 'IT직군',
-            user2ndJob: '백엔드 개발자',
-            userAddress: '부산',
-            userBirth: DateTime(2025, 2, 11),
-            userBloodType: 'A',
-            userDrinking: 'N',
-            userHeight: 180,
-            userReligion: '천주교',
-            userSmoking: 'F'),
-        [
-          Keyword(
-              kwId: 'kw132',
-              kwName: '객관',
-              kwParentId: 'kg13',
-              kwMessage: null,
-              kwLevel: '3'),
-          Keyword(
-              kwId: 'kw143',
-              kwName: '감정',
-              kwParentId: 'kg14',
-              kwMessage: null,
-              kwLevel: '3'),
-        ],
-        '나는 바보입니다.',
-      ),
-    ),
-    Profile(
-      userNo: 'US56785678',
-      name: '김개발',
-      age: '28',
-      status: '5분 전 접속',
-      distance: '3km 거리',
-      ImageList: [
-        '/images/users/US761293e0/UIbdefe04a.jpg',
-        '/images/users/US1d49ccfd/UI6aabdcd4.jpg',
-        '/images/users/US5f080b30/UI7e8f6be3.jpg'
-      ],
-      profileDetail: ProfileDetail(
-        UserInfo(
-            userNo: 'US56785678',
-            user1stJob: '마케팅',
-            user2ndJob: '콘텐츠 기획',
-            userAddress: '서울',
-            userBirth: DateTime(1996, 5, 20),
-            userBloodType: 'B',
-            userDrinking: 'Y',
-            userHeight: 175,
-            userReligion: '무교',
-            userSmoking: 'F'),
-        [
-          Keyword(
-              kwId: 'kw241',
-              kwName: '축구&농구',
-              kwParentId: 'kg24',
-              kwMessage: null,
-              kwLevel: '3'),
-          Keyword(
-              kwId: 'kw311',
-              kwName: '독립',
-              kwParentId: 'kg31',
-              kwMessage: null,
-              kwLevel: '3'),
-        ],
-        '나는 멋진 개발자입니다.',
-      ),
-    ),
-  ];
-}
